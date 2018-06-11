7f71eb7aa48ed8e94259a7f30605d47f95b48062
Robert Pierce <pierril843@gmail.com>
2018-06-11 09:24:29 -0400

    added debug infomation to main to give a quick look at the number of positions calculated

--- Source/Phase2/rosNodePublish.py
+++ Source/Phase2/rosNodePublish.py
@@ -10,6 +10,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 user = "pierril"
 lastBoxPosition = -1
+MaxCoords = 8 
 
 def ValidWaypointCheck((x,y),image):
 	leftXLimit = (y - 50)  # Box vertice based on given waypoint
@@ -306,6 +307,7 @@ if __name__ == '__main__':
 	patrolPath = []
 	convertedWaypoints = []
 	scaledWaypoints = []
+	TotalCoords = 0
 
 	rospy.init_node('patrol')
 
@@ -315,58 +317,96 @@ if __name__ == '__main__':
 	myMap = cv2.imread("/home/"+ user +"/GeneratedMaps/map5.pgm", cv2.IMREAD_GRAYSCALE)  # open map image
 
 	#Rotate map
+	print "[rotating map]"
 	rows, cols = myMap.shape
 	M = cv2.getRotationMatrix2D((cols / 2, rows / 2), 270, 1)
 	dst = cv2.warpAffine(myMap, M, (cols, rows))
 
-	print()
 	#crop map to isolate portion that is valid for pathing
+	print "[croping map]"
 	crop, thresh = CropImage(dst)
 
 	#find corner coords
 	topRightCoord = TopRightWaypoint(crop)
 	if (topRightCoord != -1):
 		patrolPath.append(topRightCoord)
+		TotalCoords += 1
+	else:
+		print "[E] - Top Right Coord Not Added"
 
 	bottomRightCoord = BottomRightWaypoint(crop)
 	if (bottomRightCoord != -1):
 	 	patrolPath.append(bottomRightCoord)
+		TotalCoords += 1
+	else:
+		print "[E] - Bottom Right Coord Not Added"
 
 	bottomLeftCoord = BottomLeftWaypoint(crop)
 	if (bottomLeftCoord != -1):
 		patrolPath.append(bottomLeftCoord)
+		TotalCoords += 1
+	else:
+		print "[E] - Bottom Left Coord Not Added"
 
 	topLeftCoord = TopLeftWaypoint(crop)
 	if (topLeftCoord != -1):
 		patrolPath.append(topLeftCoord)
+		TotalCoords += 1
+	else:
+		print "[E] - Top Left Coord Not Added"
 
 	#find midpoint coords
 	if (topRightCoord != -1 and bottomRightCoord != -1):
 		rightCoord = FindRightWaypoints((bottomRightCoord), (topRightCoord), crop)
 		if (rightCoord != -1):
 			patrolPath[1:1] = [rightCoord]
+			TotalCoords += 1
+		else:
+			print "[E] - Cal Right Coord Midpoint Not Added"
+	else:
+		print "[E] - Right Midpoints Coord Not Calculated, Dependency Missing"
 
 	if (bottomLeftCoord != -1 and bottomRightCoord != -1):
 		bottomCoord = FindBottomWaypoints((bottomLeftCoord), (bottomRightCoord), crop)
 		if (bottomCoord != -1):
 			patrolPath[3:3] = [bottomCoord]
+			TotalCoords += 1
+		else:
+			print "[E] - Cal Bottom Coord Midpoint Not Added"
+	else:
+		print "[E] - Bottom Midpoints Coord Not Calculated, Dependency Missing"
 
 	if (bottomLeftCoord != -1 and topLeftCoord != -1):
 		leftCoord = FindLeftWaypoints((bottomLeftCoord), (topLeftCoord), crop)
 		if (leftCoord != -1):
 			patrolPath[5:5] = [leftCoord]
+			TotalCoords += 1
+		else:
+			print "[E] - Cal Left Coord Midpoint Not Added"
+	else:
+		print "[E] - left Midpoints Coord Not Calculated, Dependency Missing"
 
 	if (topRightCoord != -1 and topLeftCoord != -1):
 		topCoord = FindTopWaypoints((topLeftCoord), (topRightCoord), crop)
 		if (topCoord != -1):
 			patrolPath[7:7] = [topCoord]
+			TotalCoords += 1
+		else:
+			print "[E] - Cal Top Coord Midpoint Not Added"
+	else:
+		print "[E] - Top Midpoints Coord Not Calculated, Dependency Missing"
+	
+	if TotalCoords < MaxCoords:
+		print "[I] - patrol will only include " + str(TotalCoords) + " coords, out of " + str(MaxCoords) + " coords"
+	else:
+		print "[I] - patrol will include all" + str(TotalCoords) + "Coords"
 
 	#scale waypoints from cropped coords to image coords
 	scaledWaypoints = ScaleWaypoints(dst, crop, thresh, patrolPath)
 
 	#DisplayPatrolPath(crop, patrolPath)
-	DisplayPatrolPath(dst, scaledWaypoints)
-    #DisplayPatrolPath(crop, patrolPath)
+	DisplayPatrolPa<th(dst, scaledWaypoints)
+	#DisplayPatrolPath(crop, patrolPath)
 
 
 	#convert waypoints from map coords to world coords
